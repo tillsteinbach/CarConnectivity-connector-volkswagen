ff527d839c3913ca411d6aa0d21834892e59edc7
Till Steinbach <steinbach.till@gmx.de>
2026-01-04 10:49:09 +0100

    just some more typeinfo

--- src/carconnectivity_connectors/volkswagen/connector.py
+++ src/carconnectivity_connectors/volkswagen/connector.py
@@ -87,8 +87,8 @@ class Connector(BaseConnector):
         self._background_thread: Optional[threading.Thread] = None
         self._stop_event = threading.Event()
 
-        self.connection_state: EnumAttribute = EnumAttribute(name="connection_state", parent=self, value_type=ConnectionState,
-                                                             value=ConnectionState.DISCONNECTED, tags={'connector_custom'})
+        self.connection_state: EnumAttribute[ConnectionState] = EnumAttribute(name="connection_state", parent=self, value_type=ConnectionState,
+                                                                              value=ConnectionState.DISCONNECTED, tags={'connector_custom'})
         self.interval: DurationAttribute = DurationAttribute(name="interval", parent=self, tags={'connector_custom'})
         self.interval.minimum = timedelta(seconds=180)
         self.interval._is_changeable = True  # pylint: disable=protected-access
